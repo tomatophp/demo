c0ed6aec350aa5186c20eccc929dd8bf1fad8292
Fady Mondy <info@3x1.io>
2024-02-12 16:17:25 +0200

    update demo to be SaaS app

--- config/tenancy.php
+++ config/tenancy.php
@@ -50,7 +50,7 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'tenant',
+        'prefix' => 'tomato_',
         'suffix' => '',
 
         /**
